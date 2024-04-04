5c35f5a25b07ae64a59229091e9ea53ab268cc7f
Li Fei <56876213+lifeiny@users.noreply.github.com>
2024-04-04 22:37:28 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 export const SITE = {
 	title: 'TEST',
-	author: 'Tw93',
+	author: 'LiFei',
 	description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
 	keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
 	icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
@@ -8,6 +8,6 @@ export const SITE = {
 	homePage: 'https://lifeiny.github.io/',
 	blogPage: 'https://lifeiny.github.io/',
 	twitterId: 'HiTw93',
-	githubId: 'tw93',
+	githubId: 'lifeiny',
 	repo: 'lifeiny/plog',
 };
