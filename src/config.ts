f2cfe88e219ddb028b1f1cf464c16e168ec2a348
Li Fei <56876213+lifeiny@users.noreply.github.com>
2024-04-04 22:34:40 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 export const SITE = {
-	title: '潮流周刊',
+	title: 'TEST',
 	author: 'Tw93',
 	description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
 	keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
 	icon: 'https://gw.alicdn.com/imgextra/i2/O1CN01m9YYjS1QBeW5DOm3I_!!6000000001938-2-tps-400-400.png',
 	pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
 	homePage: 'https://lifeiny.github.io/',
-	blogPage: 'https://tw93.fun/',
+	blogPage: 'https://lifeiny.github.io/',
 	twitterId: 'HiTw93',
 	githubId: 'tw93',
-	repo: 'tw93/weekly',
+	repo: 'lifeiny/plog',
 };
