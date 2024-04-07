d87101aced991b853d62c234cf1b1cbd7fc01bea
FeiLi <feili@FeiLiMacBook-Pro.local>
2024-04-07 11:50:25 +0800

    change title

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-	title: 'TEST',
+	title: 'Li Fei',
 	author: 'LiFei',
 	description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
 	keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
