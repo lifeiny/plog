e74009ff31665ce428daef0d7cd49800c87e371c
FeiLi <feili@FeiLiMacBook-Pro.local>
2024-04-08 21:11:23 +0800

    Change Background

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ export const SITE = {
 	description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
 	keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
 	icon: 'https://cdn.jsdelivr.net/gh/lifeiny/imageField/icon.png',
-	pic: 'https://gw.alicdn.com/imgextra/i4/O1CN010j3h201UDilmn716N_!!6000000002484-2-tps-1280-640.png',
+	pic: 'https://cdn.jsdelivr.net/gh/lifeiny/imageField/background.PNG',
 	homePage: 'https://lifeiny.github.io/',
 	blogPage: 'https://lifeiny.github.io/',
 	twitterId: 'HiTw93',
