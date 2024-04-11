78e9836281453ddb637fe9ee8ed8049ebe015c3d
FeiLi <feili@FeiLiMacBook-Pro.local>
2024-04-11 13:13:32 +0800

    edit icon

--- src/config.ts
+++ src/config.ts
@@ -3,7 +3,7 @@ export const SITE = {
 	author: 'LiFei',
 	description: '记录工程师 Tw93 的不枯燥生活，每周一发布，欢迎关注',
 	keywords: 'Tw93,Weekly,Blog,前端,macOS,MiaoYan,Pack,Swift',
-	icon: 'https://cdn.jsdelivr.net/gh/lifeiny/imageField/icon.png',
+	icon: 'https://cdn.jsdelivr.net/gh/lifeiny/imageField/newicon.png',
 	pic: 'https://cdn.jsdelivr.net/gh/lifeiny/imageField/background.PNG',
 	homePage: 'https://lifeiny.github.io/',
 	blogPage: 'https://lifeiny.github.io/',
